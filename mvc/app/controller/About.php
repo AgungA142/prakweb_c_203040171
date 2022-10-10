c4d4655f90939f0326834f134b91978aacb60cd9
sacred142 <agung.alfatah43@gmail.com>
2022-10-10 12:11:48 +0700

    menyelesaika videon view

--- mvc/app/controller/About.php
+++ mvc/app/controller/About.php
@@ -1,12 +1,22 @@
 <?php
 
-class About{
+class About extends Controller{
     public function page() {
-        echo'about/page';
+        $data['judul'] = 'halaman page';
+        $this-> view('templates/header', $data);
+        $this->view('about/page');
+        $this-> view('templates/footer', $data);
     }
 
-    public function index($nama = 'agung', $pekerjaan ='mahasiswa') {
-        echo "nama saya $nama, saya seorang $pekerjaan";
+    public function index($nama = 'agung', $pekerjaan ='mahasiswa', $umur = '32') {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'halaman about';
+      
+        $this-> view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this-> view('templates/footer', $data);
     }
 }
 
